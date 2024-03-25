81baf97761d632824632a10ca97494ee22259385
Ardenius <160415696+ArdeniusAI@users.noreply.github.com>
2024-03-25 23:40:07 +0300

    Update CPlus_control_box.py
    error fix: 
    removed clip since its creating a an infinite loop with the positive and negative inputs and outputs so its either clip or prompts. 
    added: 
    -latent width and height to be separate from width and height that you can now use for other nodes.
    - rearranged inputs and outputs for easier usability

--- CPlus_control_box.py
+++ CPlus_control_box.py
@@ -1,13 +1,16 @@
 """
-@author: AMA
+@author: initials AMA
 @title: Ardenius
 @nickname: Ardenius
 @description: CPlus control box is designed to gather workflow variables into 1 node.
 """
 #  this software and code © 2024 is licensed under CC BY-NC 4.0 Deed Attribution-NonCommercial 4.0 International https://creativecommons.org/licenses/by-nc/4.0/
-#  ( owner contact information ardenius7@gmail.com attribution link https://ko-fi.com/ardenius )
-#  FREE FOR PERSONAL USE visit https://ko-fi.com/ardenius
-#  and is based upon code licensed under General Public License v3.0 https://www.gnu.org/licenses/gpl-3.0.txt by 
+#  ( author contact information ardenius7@gmail.com attribution link https://ko-fi.com/ardenius )
+#  FREE FOR PERSONAL USE
+#  ➡️ follow me at https://ko-fi.com/ardenius on the top right corner (follow)
+#  📸 Change the mood ! by Visiting my AI Image Gallery
+#  🏆 Support me by getting Premium Memebers only Perks (Premium SD Models, ComfyUI custom nodees, and more to come)
+#  below code is based upon ComfyUI code licensed under General Public License v3.0 https://www.gnu.org/licenses/gpl-3.0.txt by
 #  contributers found here https://github.com/comfyanonymous/ComfyUI
 #  thus all code here is released to the user as per the GNU V3.0 terms.
 
@@ -33,9 +36,6 @@ class Control_Box:
     def INPUT_TYPES(s):
         return {"required":
                     {
-                    "width": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
-                    "height": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
-                    "latent_batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
                     "cfg": ("FLOAT", {"default": 8, "min": 0.1, "max": 15, "step": 0.1}),
                     "steps": ("INT", {"default": 20, "min": 1, "max": 100, "step": 1}),
                     "denoise": ("FLOAT", {"default": 1.0, "min": 0.01, "max": 1.0, "step": 0.01}),
@@ -44,13 +44,17 @@ class Control_Box:
                     "positive_prompt": ("CONDITIONING", {"default": ""}),
                     "negative_prompt": ("CONDITIONING", {"default": ""}),
                     "model": ("MODEL",),
-                    "clip": ("CLIP",),
                     "vae": ("VAE",),
+                    "width": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
+                    "height": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
+                    "latent_width": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
+                    "latent_height": ("INT", {"default": 1024, "min": 64, "max": MAX_RESOLUTION, "step": 8}),
+                    "latent_batch_size": ("INT", {"default": 1, "min": 1, "max": 4096}),
                     },
                 }
 
-    RETURN_NAMES = ("model", "positive", "negative", "latent_out", "seed", "cfg", "steps", "denoise", "scaler", "clip", "vae", "width", "height",)
-    RETURN_TYPES = ("MODEL", "CONDITIONING", "CONDITIONING", "LATENT", "INT", "FLOAT", "INT", "FLOAT", "FLOAT", "CLIP", "VAE", "INT", "INT",)
+    RETURN_NAMES = ("model", "positive", "negative", "latent_out", "seed", "cfg", "steps", "denoise", "scaler", "vae", "width", "height", "latent_width", "latent_height",)
+    RETURN_TYPES = ("MODEL", "CONDITIONING", "CONDITIONING", "LATENT", "INT", "FLOAT", "INT", "FLOAT", "FLOAT", "VAE", "INT", "INT", "INT", "INT",)
 
     FUNCTION = "output_function"
 
@@ -58,7 +62,7 @@ class Control_Box:
 
     CATEGORY = "CPlus"
 
-    def output_function(self, width, height, latent_batch_size, cfg, steps, denoise, scaler, seed, positive_prompt, negative_prompt, model, clip, vae):
+    def output_function(self, cfg, steps, denoise, scaler, seed, positive_prompt, negative_prompt, model, vae, width, height, latent_width, latent_height, latent_batch_size):
 
         remainder = width % 8
         width = width + remainder
@@ -66,9 +70,15 @@ class Control_Box:
         remainder = height % 8
         height = height + remainder
 
-        latent_out = self.generate_latent(width, height, latent_batch_size)
+        remainder = latent_width % 8
+        latent_width = latent_width + remainder
 
-        return model, positive_prompt, negative_prompt, latent_out, seed, cfg, steps, denoise, scaler, clip, vae, width, height
+        remainder = latent_height % 8
+        latent_height = latent_height + remainder
+
+        latent_out = self.generate_latent(latent_width, latent_height, latent_batch_size)
+
+        return model, positive_prompt, negative_prompt, latent_out, seed, cfg, steps, denoise, scaler, vae, width, height, latent_width, latent_height
 
     def generate_latent(self, width, height, latent_batch_size=1):
         latent = torch.zeros([latent_batch_size, 4, height // 8, width // 8], device=self.device)
